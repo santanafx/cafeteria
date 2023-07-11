0523507fa06953613826b7a0218a4bb7bf0a561c
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-07-11 13:16:11 -0300

    Novas pastas do projeto e novos arquivos Home.tsx e Header.tsx
    Para iniciar o projeto, foi criado a pagina 'Home.tsx' utilizando o pacote react-router-dom, e foi criado o componente 'Header.tsx' e o partial de Header.tsx chamado _header.scss

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="headerContainer">
+      <nav>
+        <div className="headerImgContainer">
+          <img src="./images/logo.jpg" alt="Logo da cafeteria." />
+        </div>
+        <div className="headerMenuOpt">
+          <a href="">Home</a>
+          <a href="">About us</a>
+          <a href="">Menu</a>
+          <a href="">Our Chef</a>
+          <a href="">Reservation</a>
+          <a href="">Contact us</a>
+        </div>
+        <div className="headerReservation">
+          <a href="">Reservation</a>
+        </div>
+      </nav>
+    </header>
+  );
+};
