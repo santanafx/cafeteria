eb7d0b9960b8ea7df6001726e7e2c2c1e02c1c6d
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-07-12 18:31:01 -0300

    Novo componente 'SectionNumbers.tsx'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import { Header } from "../components/Header";
 import { SectionAbout } from "../components/SectionAbout";
 import { SectionDelicious } from "../components/SectionDelicious";
+import { SectionDiscover } from "../components/SectionDiscover";
 import { SectionFood } from "../components/SectionFood";
+import { SectionNumbers } from "../components/SectionNumbers";
 
 export const Home = () => {
   return (
@@ -11,6 +13,8 @@ export const Home = () => {
         <SectionDelicious />
         <SectionAbout />
         <SectionFood />
+        <SectionDiscover />
+        <SectionNumbers />
       </main>
     </>
   );
