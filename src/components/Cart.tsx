52ea8b37f77a4194c1ecef0e1a6c6c954726d870
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-07-16 17:13:19 -0300

    Corrigido alguns bugs de responsividade e adicionado arquivo README ao projeto

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,32 +3,39 @@ import { comidasListaObj } from "../data/ComidasLista";
 import { CartItem } from "./CartItem";
 
 interface CartProps {
-  openCart: boolean;
-  handleOpenCart: (openCart: boolean) => void;
+    openCart: boolean;
+    handleOpenCart: (openCart: boolean) => void;
 }
 
 export const Cart = ({ openCart, handleOpenCart }: CartProps) => {
-  const { cartItems } = useShoppingCart();
-  return (
-    <div className="cartContainer">
-      <button onClick={() => handleOpenCart(openCart)}>X</button>
-      <div>
-        {cartItems.map((item) => (
-          <CartItem key={JSON.stringify(item)} {...item} />
-        ))}
-        <div className="cartPrecoTotal">
-          <span>
-            Total: $
-            {cartItems.reduce((total, cartItem) => {
-              const item = comidasListaObj.find((i) => i.id === cartItem.id);
-              return total + (item?.preco || 0) * cartItem.quantity;
-            }, 0)}
-          </span>
+    const { cartItems } = useShoppingCart();
+    return (
+        <div className="cartContainer">
+            <button onClick={() => handleOpenCart(openCart)}>X</button>
+            <div>
+                {cartItems.map((item) => (
+                    <CartItem key={JSON.stringify(item)} {...item} />
+                ))}
+                <div className="cartPrecoTotal">
+                    <span>
+                        Total: $
+                        {cartItems
+                            .reduce((total, cartItem) => {
+                                const item = comidasListaObj.find(
+                                    (i) => i.id === cartItem.id
+                                );
+                                return (
+                                    total +
+                                    (item?.preco || 0) * cartItem.quantity
+                                );
+                            }, 0)
+                            .toFixed(2)}
+                    </span>
+                </div>
+                <div className="cartButtonOrder">
+                    <button>Order</button>
+                </div>
+            </div>
         </div>
-        <div className="cartButtonOrder">
-          <button>Order</button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
