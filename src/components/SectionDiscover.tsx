a6e2133326584efa8c2ab0ecb4dc4dc82c7774a7
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-07-15 23:32:52 -0300

    Corrigindo responsividade componentes

--- src/components/SectionDiscover.tsx
+++ src/components/SectionDiscover.tsx
@@ -1,26 +1,26 @@
 import { Button } from "./Button";
 
 export const SectionDiscover = () => {
-  return (
-    <section className="discoverCafeteriaBg">
-      <div className="discoverCafeteriaContainer">
-        <div className="discoverCafeteriaService">
-          <h4>Discover More</h4>
-          <h1>High Professional Service</h1>
-          <p>
-            Morbi pharetr se id lectus iaculis, nec commodo mauris interdum.
-            Quisque ipsum neque, ullamcorper in diam nec.
-          </p>
-          <ul>
-            <li> All the lorem ipsum generators on the Internet</li>
-            <li> When an unknown printer took a galley</li>
-            <li> Various versions have evolved years</li>
-          </ul>
-          <Button text={"Discover more"} />
-        </div>
-        <div className="discoverEmptyContainer"></div>
-        <div className="discoverCafeteriaImg"></div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="discoverCafeteriaBg">
+            <div className="discoverCafeteriaContainer">
+                <div className="discoverCafeteriaService">
+                    <h4>Discover More</h4>
+                    <h1>High Professional Service</h1>
+                    <p>
+                        Morbi pharetr se id lectus iaculis, nec commodo mauris
+                        interdum. Quisque ipsum neque, ullamcorper in diam nec.
+                    </p>
+                    <ul>
+                        <li> All the lorem ipsum generators on the Internet</li>
+                        <li> When an unknown printer took a galley</li>
+                        <li> Various versions have evolved years</li>
+                    </ul>
+                    <Button text={"Discover more"} />
+                </div>
+                <div className="discoverEmptyContainer"></div>
+                <div className="discoverCafeteriaImg"></div>
+            </div>
+        </section>
+    );
 };
--- src/components/SectionReview.tsx
+++ src/components/SectionReview.tsx
@@ -1,22 +1,25 @@
 export const SectionReview = () => {
-  return (
-    <section className="reviewCafeteriaBg">
-      <div className="reviewCafeteriaContainer">
-        <div className="reviewQuote">
-          <span>”</span>
-        </div>
-        <div className="reviewCafeteriaText">
-          <h2>
-            This is due to their excellent service, competitive pricing and
-            customer support. It’s throughly refresing to get such a personal
-            touch.
-          </h2>
-        </div>
-        <div className="reviewCafeteriaImg">
-          <img src="./images/customer.webp" alt="Foto de um cliente." />
-        </div>
-        <h3>Albert Doe</h3>
-      </div>
-    </section>
-  );
+    return (
+        <section className="reviewCafeteriaBg">
+            <div className="reviewCafeteriaContainer">
+                <div className="reviewQuote">
+                    <span>”</span>
+                </div>
+                <div className="reviewCafeteriaText">
+                    <h2>
+                        This is due to their excellent service, competitive
+                        pricing and customer support. It’s throughly refresing
+                        to get such a personal touch.
+                    </h2>
+                </div>
+                <div className="reviewCafeteriaImg">
+                    <img
+                        src="./images/customer.webp"
+                        alt="Foto de um cliente."
+                    />
+                </div>
+                <h3>Albert Doe</h3>
+            </div>
+        </section>
+    );
 };
